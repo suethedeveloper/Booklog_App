82619ee47dfb739d05a6f3208677ff9c83b630c7
Sue Lucas <choi.soyoung@gmail.com>
2015-12-03 10:14:30 -0800

    Minor text change ;

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Creating Home Page" do
 
     scenario do
         visit "/"
-        expect(page).to have_link("Bookworm Brady")
+        expect(page).to have_link("Bookworm")
         expect(page).to have_link("Home")
         expect(page).to have_content("Book Reading Lounge")
     end
