8db2cfd1731a8759ff4cdf638c61f88b4ba2b3c5
Sue Lucas <choi.soyoung@gmail.com>
2015-12-03 10:17:21 -0800

    Fix flash messages bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,7 @@ module ApplicationHelper
       |msg| content_tag(:li, msg) }.join
       html = <<-HTML
 <div class="alert alert-error alert-danger">
-  <a href='#' class='close' data-dismiss='alert' &#215;></a>
+  <a href='#' class='close' data-dismiss='alert'>&#215;</a>
     <%= content_tag :div, msg if msg.is_a?(String) %>
 </div>
 HTML
